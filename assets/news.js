28417a647513b4d6378b55ab6d84354d59b1c300
nsangsiri <prabataroh@gmail.com>
2025-08-19 21:26:16 +0800

    prab

---
+++ assets/news.js
@@ -0,0 +1,17 @@
+// --- CONTENT MANAGEMENT: NEWS ---
+// To add a new news item, add a new object to this array.
+
+const newsItems = [
+    {
+        date: 'Aug 2025',
+        content: 'Start my PhD in <a href="https://www.cse.cuhk.edu.hk/" class="font-semibold text-green-700 hover:underline">Department of Computer Science and Engineering</a> at CUHK!'
+    },
+    // {
+    //     date: 'May 2024',
+    //     content: 'I will be joining <a href="#" class="font-semibold text-green-700 hover:underline">[Company/Lab Name]</a> as a Research Intern this summer.'
+    // },
+    // {
+    //     date: 'Jan 2024',
+    //     content: 'Presented my work on [Project Name] at the [University/Department Symposium].'
+    // },
+];
