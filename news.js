57391c95c771db7900fdf841a7e8e58ad7189cce
nsangsiri <prabataroh@gmail.com>
2025-08-19 15:36:09 +0800

    prab

---
+++ news.js
@@ -0,0 +1,17 @@
+// --- CONTENT MANAGEMENT: NEWS ---
+// To add a new news item, add a new object to this array.
+
+const newsItems = [
+    {
+        date: 'Aug 2024',
+        content: 'Our paper on [Paper Topic] was accepted to <a href="#" class="font-semibold text-green-700 hover:underline">[Conference/Journal Name]</a>! See you in [Location]!'
+    },
+    {
+        date: 'May 2024',
+        content: 'I will be joining <a href="#" class="font-semibold text-green-700 hover:underline">[Company/Lab Name]</a> as a Research Intern this summer.'
+    },
+    {
+        date: 'Jan 2024',
+        content: 'Presented my work on [Project Name] at the [University/Department Symposium].'
+    },
+];
---
+++ publications.js
@@ -0,0 +1,25 @@
+// --- CONTENT MANAGEMENT: PUBLICATIONS ---
+// To add a new publication, add a new object to this array.
+
+const publications = [
+    {
+        authors: '<strong>Your Name</strong>, Co-author Name, Another Co-author',
+        title: 'Title of Your Amazing Publication That Solves an Important Problem',
+        venue: 'Conference on Important Discoveries (CID \'24)',
+        links: [
+            { name: 'PDF', url: '#' },
+            { name: 'DOI', url: '#' },
+            { name: 'Code', url: '#' },
+            { name: 'Project Page', url: '#' }
+        ]
+    },
+    {
+        authors: 'Author Name, <strong>Your Name</strong>, Another Author',
+        title: 'Another Great Paper on a Different but Equally Interesting Topic',
+        venue: 'Journal of Groundbreaking Research (JGR)',
+        links: [
+            { name: 'PDF', url: '#' },
+            { name: 'DOI', url: '#' }
+        ]
+    },
+];
